71bbd0bd6251f40005ec54285298c2961f53def6
Richard Clifford <richard.clifford@soluis.com>
2020-04-09 21:00:26 +0100

    safe area and edfeinsets notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.tealAccent,
+            // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+            // margin: EdgeInsets.all(40),
+            // margin: EdgeInsets.fromLTRB(30, 20, 10, 0),
+            margin: EdgeInsets.only(left: 50),
+            padding: EdgeInsets.all(20),
+            child: Text('hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
